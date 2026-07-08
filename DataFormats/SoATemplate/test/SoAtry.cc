4877ca32e137fa406fb4307fc8b98890b9ed1003
Manuel Speranza <manuelsperanza01@gmail.com>
2026-07-08 16:56:00 +0200

    simplified SoAtry.cc for analyse code

--- DataFormats/SoATemplate/test/SoAtry.cc
+++ DataFormats/SoATemplate/test/SoAtry.cc
@@ -1,47 +1,38 @@
 #include <cstdlib>
+#include <iostream>
 #include <memory>
-#include <tuple>
 
 #include "DataFormats/SoATemplate/interface/SoALayout.h"
 
-GENERATE_SOA_LAYOUT(InputLayoutTemplate,
-  SOA_COLUMN(float, x),
-  SOA_COLUMN(float, y),
-  SOA_COLUMN(float, z))
-
-GENERATE_SOA_LAYOUT(ResultLayoutTemplate,
-  SOA_COLUMN(float, a))
+GENERATE_SOA_LAYOUT(InputLayoutTemplate, SOA_COLUMN(float, x), SOA_COLUMN(float, y), SOA_COLUMN(float, z))
+GENERATE_SOA_LAYOUT(ResultLayoutTemplate, SOA_COLUMN(float, a))
 
 using InputLayout = InputLayoutTemplate<>;
 using ResultLayout = ResultLayoutTemplate<>;
 
-using ViewInput = InputLayout::View;
-using ViewResult = ResultLayout::View;
+int main() {
+  constexpr int size = 8;
 
-int main(){
+  std::unique_ptr<std::byte, decltype(std::free) *> inputMem{
+      reinterpret_cast<std::byte *>(aligned_alloc(InputLayout::alignment, InputLayout::computeDataSize(size))),
+      std::free};
+  std::unique_ptr<std::byte, decltype(std::free) *> resultMem{
+      reinterpret_cast<std::byte *>(aligned_alloc(ResultLayout::alignment, ResultLayout::computeDataSize(size))),
+      std::free};
 
-  constexpr int slSize = 128;
+  InputLayout input{inputMem.get(), size};
+  ResultLayout result{resultMem.get(), size};
 
-  const std::size_t inputBufferSize = InputLayout::computeDataSize(slSize);
-  const std::size_t resultBufferSize = ResultLayout::computeDataSize(slSize);
+  InputLayout::View inputView{input};
+  ResultLayout::View resultView{result};
 
-  std::unique_ptr<std::byte, decltype(std::free) *> inputBuffer{
-      reinterpret_cast<std::byte *>(aligned_alloc(InputLayout::alignment, inputBufferSize)), std::free};
-  std::unique_ptr<std::byte, decltype(std::free) *> resultBuffer{
-      reinterpret_cast<std::byte *>(aligned_alloc(ResultLayout::alignment, resultBufferSize)), std::free};
-
-  InputLayout input{inputBuffer.get(), slSize};
-  ResultLayout result{resultBuffer.get(), slSize};
-
-  ViewInput inputView{input};
-  ViewResult resultView{result};
-
-  for (int i = 0; i < slSize; ++i) {
-    inputView[i] = {static_cast<float>(i), static_cast<float>(i) * 2.f, static_cast<float>(i) * 3.f};
+  for (int i = 0; i < size; ++i) {
+    inputView[i] = {float(i), float(i) * 2.f, float(i) * 3.f};
+    resultView[i].a() = inputView[i].y() * inputView[i].x() + inputView[i].z();
   }
 
-  for (int i = 0; i < slSize; ++i) {
-    resultView[i].a() = inputView[i].y() * inputView[i].x() + inputView[i].z();
+  for (int i = 0; i < size; ++i) {
+    std::cout << "a[" << i << "] = " << resultView[i].a() << '\n';
   }
 
   return EXIT_SUCCESS;
